3fc4caa0255a2ed1205fc0fd926cd18ec6b9c53c
Devin Andres Salemi <devinsalem@gmail.com>
2017-07-22 07:11:49 -0700

    Add files via upload

---
+++ Client/Country.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Country.aspx.cs" Inherits="Country" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+    
+    <div>
+    
+        COUNTRY:
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    
+    </div>
+      
+    
+    
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="SUBMIT" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Client/Country.aspx.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public partial class Country : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string s = TextBox1.Text;
+
+
+        try
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 8080);
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+
+
+            writer.WriteLine(s);
+            writer.Flush();
+            string ss = reader.ReadLine();
+
+            client.Close();
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine("SocketException: {0}", ex);
+        }
+
+
+    }
+}
